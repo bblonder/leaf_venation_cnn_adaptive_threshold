a4efd563aa55f75e3a0388670f39d6b08d955b48
Satvik Sharma <satvik.sharma@berkeley.edu>
2022-05-13 13:49:32 -0500

    removed unnecessary files and added requirements.txt for python environment setup

--- plot_vein_stats.py
+++
@@ -1,3 +0,0 @@
-from cnn_updated_multi_v2 import *
-
-plot_vein_stats('f14', 'CR3', 3)
--- read_summary_exec.py
+++
@@ -1,5 +0,0 @@
-from read_summary import *
-
-make_graph("10399_crop")
-make_graph("30225A_crop")
-make_graph("ax139big_crop")
